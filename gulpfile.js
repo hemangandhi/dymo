0f80fb683b5a042ebe3c3a6bbe4ce61359f2a28d
Heman Gandhi <hemangandhi@gmail.com>
2017-10-08 02:06:15 -0400

    cleaner hacking

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,20 @@ var jshint = require('gulp-jshint');
 var mocha  = require('gulp-mocha');
 
 gulp.task('lint', function() {
-	return gulp
-		.src(['gulpfile.js', 'index.js', 'src/*.js', 'test/*.js'])
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
+  return gulp
+    .src(['gulpfile.js', 'index.js', 'src/*.js', 'test/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('test', function() {
-	return gulp
-		.src('test/*.js')
-		.pipe(mocha());
+  return gulp
+    .src('test/*.js')
+    .pipe(mocha());
 });
 
 gulp.task('default', ['lint', 'test'], function() {
-	gulp.watch(['src/*.js', 'test/*.js'], function() {
-		gulp.run('lint', 'test');
-	});
+  gulp.watch(['src/*.js', 'test/*.js'], function() {
+    gulp.run('lint', 'test');
+  });
 });
--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const xml = require('xml2json');
 
-// let window = {setTimeout : setTimeout, JSON : JSON};
-const api = require('./test.js');
-
-console.log(global);
-
-let printers = dymo.label.framework.getPrinters();
+const api = require('./dymo-frontend.js');
